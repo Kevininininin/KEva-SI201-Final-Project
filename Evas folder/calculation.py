a911991e383c6a32dcb0d035b1472cf10c997a82
Kevininininin <jinghan.wu@ufl.edu>
2025-12-11 16:22:20 -0500

    distributed work

---
+++ Evas folder/calculation.py
@@ -0,0 +1,3 @@
+# Eva's graphs
+    # Bar chart of avg delay for is_day or is_not_day
+    # bar chart for windspeed vs airline: does certain airlines experience higher or lower avg windspeed than other airlines?
---
+++ Kevins folder/calculation.py
@@ -0,0 +1,4 @@
+# Kevin's graphs
+    # Scatter plot comparing avg delay vs. wind speed
+    # Scatter plot comparing avg delay vs. humidity 
+
