05e5ee00f0887c453f9cad340f815b971ddd40fd
Eva Park <evapark@umich.edu>
2025-12-15 01:33:06 +0900

    evas calculation/bar chart1 done

--- Evas folder/calculation.py
+++ Evas folder/calculation.py
@@ -1,7 +1,7 @@
 # Eva's graphs
     # Bar chart of avg delay for is_day or is_not_day
     # bar chart for windspeed vs airline: does certain airlines experience higher or lower avg windspeed than other airlines?
-    
+
 import sqlite3
 import matplotlib.pyplot as plt
 
@@ -53,11 +53,89 @@ def get_avg_delay_by_session():
     return return_dict
 
 
+
+def add_is_day_to_avg_delay(avg_delay_dict):
+    # Connect DB
+    conn = sqlite3.connect("Database/final_project_2.db")
+    cur = conn.cursor()
+
+    # Query weather_sessions table
+    query = """
+        SELECT session_name, is_day
+        FROM weather_sessions
+    """
+    cur.execute(query)
+    rows = cur.fetchall() #list of tupples [(str session_name, int is_day)]
+
+    # Prepare return_dict
+    return_dict = {}
+
+    # Enrich avg delay data directly in one loop
+    for session_name, is_day in rows:
+        if session_name in avg_delay_dict:
+            avg_delay = avg_delay_dict[session_name]
+
+            return_dict[session_name] = {
+                "avg_delay": avg_delay,
+                "is_day": is_day
+            }
+
+    # Close DB
+    conn.close()
+
+    # Return result
+    return return_dict
+
+def compute_daynight_avg(enriched_dict):
+    day_delays = []
+    night_delays = []
+
+    for session, data in enriched_dict.items():
+        if data["is_day"] == 1:
+            day_delays.append(data["avg_delay"])
+        else:
+            night_delays.append(data["avg_delay"])
+
+    # calculate average delays by day and night
+    avg_day = sum(day_delays) / len(day_delays)
+    avg_night = sum(night_delays) / len(night_delays)
+
+    return {"Day": avg_day, "Night": avg_night}
+
+# ========== PLOTTING ==========
+# Bar chart 1: avg delay vs. is_day
+
+def plot_daynight_delay(avg_dict):
+    labels = list(avg_dict.keys())     # ["Day", "Night"]
+    values = list(avg_dict.values())   # [avg_day, avg_night]
+
+    plt.figure()
+    plt.bar(labels, values, color=["gold", "navy"])
+    plt.title("Average Flight Delay: Day vs Night")
+    plt.ylabel("Average Delay (minutes)")
+    plt.xlabel("Time of Day")
+    plt.tight_layout()
+    # plt.show()
+    plt.savefig("Evas folder/delay_vs_is_day.png")
+    plt.close()
+
+
+
 # ========== MAIN FUNCTION ==========
 def main():
     # Calculating delay
     avg_delays = get_avg_delay_by_session()
     print(avg_delays)
+
+    # Collect is_data data to add to avg_delays dict
+    avg_delay_enriched= add_is_day_to_avg_delay(avg_delays)
+    print(avg_delay_enriched)
+
+    computed_dict=compute_daynight_avg(avg_delay_enriched)
+
+    plot_daynight_delay(computed_dict)
+
+    
 
 
 if __name__ == "__main__":
